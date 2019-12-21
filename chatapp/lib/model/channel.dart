2b2e530b3261518231c04ad5105111b57143db53
Miikka Tervo <miikka.tervo@gmail.com>
2019-12-21 17:15:59 +0200

    messageItem

--- chatapp/lib/model/channel.dart
+++ chatapp/lib/model/channel.dart
@@ -1,15 +1,16 @@
 class Channel {
+  String id;
   String image;
   String name;
-  Channel({this.image, this.name});
+  Channel({this.id, this.image, this.name});
 }
 
 var channels = [
-  Channel(image: "assets/images/yle1.png", name: "Yle 1"),
-  Channel(image: "assets/images/yle2.png", name: "Yle 2"),
-  Channel(image: "assets/images/mtv3.png", name: "Mtv 3"),
-  Channel(image: "assets/images/nelonen.png", name: "Nelonen"),
-  Channel(image: "assets/images/sub.png", name: "Sub"),
-  Channel(image: "assets/images/tv5.png", name: "TV 5"),
-  Channel(image: "assets/images/fox.png", name: "Fox"),
+  Channel(id: "0", image: "assets/images/yle1.png", name: "Yle 1", ),
+  Channel(id: "1", image: "assets/images/yle2.png", name: "Yle 2"),
+  Channel(id: "2", image: "assets/images/mtv3.png", name: "Mtv 3"),
+  Channel(id: "3", image: "assets/images/nelonen.png", name: "Nelonen"),
+  Channel(id: "4", image: "assets/images/sub.png", name: "Sub"),
+  Channel(id: "5", image: "assets/images/tv5.png", name: "TV 5"),
+  Channel(id: "6", image: "assets/images/fox.png", name: "Fox"),
 ];
--- chatapp/lib/widgets/ChatScreen.dart
+++ chatapp/lib/widgets/ChatScreen.dart
@@ -1,46 +1,6 @@
 import 'package:flutter/material.dart';
 import '../model/message.dart';
-
-class MessageItem extends StatelessWidget {
-  final String user;
-  final String message;
-  final List<int> color;
-  MessageItem({this.user, this.message, this.color});
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-                  children: <Widget>[
-                    Text(
-                      user,
-                      style: TextStyle(
-                        fontFamily: 'Nunito',
-                        fontSize: 18,
-                        color: Color.fromRGBO(color[0], color[1], color[2], 1.0)
-                      ),
-                    ),
-                    Text(
-                      ":",
-                      style: TextStyle(
-                        fontFamily: 'Nunito',
-                        fontSize: 18,
-                        color: Colors.black
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 5),
-                    ),
-                    Text(
-                      message,
-                      style: TextStyle(
-                        fontFamily: 'Nunito',
-                        fontSize: 18,
-                        color: Colors.grey[800]
-                      ),
-                    ),
-                  ],
-                );
-  }
-}
+import './MessageItem.dart';
 
 class ChatScreen extends StatelessWidget {
   final String name;
